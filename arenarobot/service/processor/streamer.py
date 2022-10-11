3290805b1d314c8f3c7a120911cf243cf94c1161
johnpedraza <jpedraza1123@gmail.com>
2022-10-10 21:37:01 -0400

    fix streamer gui updates partly

--- arenarobot/service/processor/streamer.py
+++ arenarobot/service/processor/streamer.py
@@ -40,24 +40,16 @@ class ArenaRobotServiceProcessorStreamer(ArenaRobotServiceProcessor):
         self.filter_topic = filter_topic
         self.fetched_once = False
         
-        # make a box
-        self.scene = Scene(host="mqtt.arenaxr.org", scene="origin_only")
-        self.box = Box(object_id="myBox", position=(0,3,-3), scale=(0.05,0.05,0.05))
         
         # initialize some variables
         self.hvac_hud_switch = False
         self.hvac_stats = None
         # self.plug_status, self.plug_stats = get_smartplug_stats()
         self.plug_hud_switch = False
-
+        self.box = None
         self.vav_stat_button = None
         self.stat_panel = None
         self.stat_text = None
-        self.airflow_max = None
-        self.plug_stat_panel = None
-        self.plug_stat_text = None
-
-        self.last_activitated = 0
 
         super().__init__(
             device_instance_processor_type=processor_type,
@@ -65,91 +57,49 @@ class ArenaRobotServiceProcessorStreamer(ArenaRobotServiceProcessor):
             **kwargs
             )
 
+
+    def stat_panel_trigger(scene, evt, msg):     
+        if evt.type  == "mousedown":
+            self.hvac_hud_switch = not self.hvac_hud_switch
+            print("tracker hud button:", self.hvac_hud_switch)
+            self.stat_panel.data.visible=self.hvac_hud_switch
+            scene.update_object(self.stat_panel)
+    
     def setup(self):
-        # add the box to ARENA
-        self.scene.add_object(self.box)
-        
-        self.vav_stat_button = Box(
-            object_id="HUD_button", 
-            position=(-0.834, 2.7, -1.153),
-            scale=Scale(.15,.05,.15), 
-            clickable=True, 
-            evt_handler=stat_panel_trigger,
-            color="#FF0000",
-            persist=True)
+        # make a box
+        self.scene = Scene(host="mqtt.arenaxr.org", scene="demo")
+        self.box = Box(
+            object_id="tracker", position=(0,3,-3), scale=(0.05,0.05,0.05),
+            clickable=True,
+            evt_handler=self.stat_panel_trigger
+        )
 
         self.stat_panel = Object(**{
-            "object_id": "stat_panel",
+            "object_id": "tracker_stat_panel",
             "persist": True,
             "data": {
                 "panel": {"height": 1.4, "width": 1.1, "depth":0.05},
-                "position": (-0.834, 1.814, -1.153),
+                "position": (0, 0, -1.153),
                 "rotation": (0, 0, 0),
                 "look-at": "#my-camera",
-                "parent": "myBox",
+                "parent": "tracker",
                 "visible": False
             }
         })
         
         self.stat_text = Text(**{
-            "object_id":"stat_text","persist":True,"type":"object","action":"update",
+            "object_id":"tracker_stat_text","persist":True,"type":"object","action":"update",
             "data":
                 {"object_type":"text",
                 "position":{"x":0.135,"y":0.67,"z":0},"rotation":{"x":0,"y":0,"z":0},
                 "font":"roboto","side":"front","text":self.hvac_stats,"width":1.3,"color":"#ffffff",
-                "parent":"stat_panel",
+                "parent":"tracker_stat_panel",
                 "baseline":"top","align":"left","xOffset":0.03,"zOffset":0.03}
             })
 
-
-        self.plug_stat_panel = Object(**{
-                "object_id": "plug_stat_panel",
-                "persist": True,
-                "data": {
-                    "panel": {"height": 0.45, "width": 1, "depth":0.05},
-                    "position": (0, 0.5, 0),
-                    "rotation": (0, 0, 0),
-                    "look-at": "#my-camera",
-                    "visible": False,
-                    "parent": "water_cooler_button"
-                }
-            })
-        
-        self.plug_stat_text = Text(**{
-            "object_id":"plug_stat_text","persist":True,"type":"object","action":"update",
-            "data":
-                {"object_type":"text",
-                "position":{"x":0.200,"y":0.18,"z":0},"rotation":{"x":0,"y":0,"z":0},
-                "font":"roboto","side":"front","text":self.plug_stats,"width":1.3, "color":"#ffffff",
-                "parent":"plug_stat_panel",
-                "baseline":"top","align":"left","xOffset":0.03,"zOffset":0.03}
-            })
-
-
-        self.scene.add_object(self.vav_stat_button)
         self.scene.add_object(self.stat_panel)
         self.scene.add_object(self.stat_text)
-        self.scene.add_object(self.airflow_input)
-        self.scene.add_object(self.airflow_revert)
-        self.scene.add_object(self.temp_input)
-        self.scene.add_object(self.temp_revert)
-
-        self.scene.add_object(self.water_cooler_button)
-        self.scene.add_object(self.plug_stat_panel)
-        self.scene.add_object(self.plug_stat_text)
-        self.scene.add_object(self.plug_on_button)
-        self.scene.add_object(self.plug_off_button)
-
-
-        def stat_panel_trigger(scene, evt, msg):
-            ## Get Event type
-            if evt.type  == "mousedown":
-                self.hvac_hud_switch = not self.hvac_hud_switch
-                print(self.hvac_hud_switch)
-                self.stat_panel.data.visible=self.hvac_hud_switch
-                scene.update_object(self.stat_panel)
-                # scene.update_object(airflow_max, **{"click-listener": False})
-                
+        self.scene.add_object(self.box)
         # Airspeed
         self.Bus = smbus.SMBus(1)
         
@@ -224,3 +174,6 @@ class TransformedStreamerJSONEncoder(JSONEncoder):
             return list(o)
 
         return JSONEncoder.default(self, o)
+
+
+
